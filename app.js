b7e2495dc65f97ec72e3126286067f563e4f0a74
vlakoosh <vlakoosh@gmail.com>
2024-05-08 00:48:28 +0200

    INSANE OPTIMIZATION. 4fps -> 60+fps using imageData buffer. game now runs at smooth 60fps.
    Signed-off-by: Szymon Walkusz <szymon.walkusz@student.kdg.be>

--- app.js
+++ app.js
@@ -26,21 +26,23 @@ class trackVector{
 }
 
 let track = [new trackVector(0, 200)];
-track.push(new trackVector(1, 200));
-track.push(new trackVector(0, 200));
-track.push(new trackVector(-1, 200));
-track.push(new trackVector(1, 200));
-track.push(new trackVector(0, 200));
-track.push(new trackVector(0.3, 200));
-track.push(new trackVector(-1, 200));
-track.push(new trackVector(0.3, 200));
-track.push(new trackVector(-0.5, 200));
+track.push(new trackVector(1, 2000));
+track.push(new trackVector(0, 2000));
+track.push(new trackVector(-1, 2000));
+track.push(new trackVector(1, 2000));
+track.push(new trackVector(0, 2000));
+track.push(new trackVector(0.3, 2000));
+track.push(new trackVector(-1, 2000));
+track.push(new trackVector(0.3, 2000));
+track.push(new trackVector(-0.5, 2000));
 
 let trackOffset = 0;
 let trackSection = 0;
 let targetCurvature = 0;
 let currentCurvature = 0;
 
+let bufferData = ctx.createImageData(SCREEN_WIDTH, SCREEN_HEIGHT);
+let buffer = bufferData.data;
 
 const grass = ctx.createImageData(1,1); // only do this once per page
 d  = grass.data;
@@ -107,8 +109,8 @@ function updateScreen() {
     for (let y = 0; y < SCREEN_HEIGHT; y ++)
     {
         let perspective = y / (SCREEN_HEIGHT / 2);
-        let grassColor = Math.sin((20 * Math.pow(1-perspective, 3)) + playerDistance * 0.0001);
-        let curbColor = Math.sin(80 * (Math.pow(1-perspective, 3)) + playerDistance * 0.0001);
+        let grassColor = Math.sin((20 * Math.pow(1-perspective, 3)) + playerDistance * 0.00003);
+        let curbColor = Math.sin(80 * (Math.pow(1-perspective, 3)) + playerDistance * 0.00003);
 
         for (let x = 0; x < SCREEN_WIDTH; x++)
         {
@@ -116,7 +118,8 @@ function updateScreen() {
 
             let roadMiddlePoint = 0.5 + currentCurvature * Math.pow((1-perspective), 3);
             let curvatureDifference = targetCurvature - currentCurvature;
-            currentCurvature += curvatureDifference * 0.00001;
+            currentCurvature += curvatureDifference * 0.0000003;
+            playerPosition -= currentCurvature * 0.000003;
             //0.1 - minimum road width
             let roadWidth = 0.1 + perspective * 0.8;
             let curbWidth = roadWidth * 0.15;
@@ -148,35 +151,53 @@ function updateScreen() {
 
             //the random >=/<=/+1 are for fixing empty pixels on places where x rounds
             if (x >= 0 && x < leftGrassBound) {
-                ctx.putImageData(currentGrass, x, row);
+                buffer[(x + row*SCREEN_WIDTH)*4] = currentGrass.data[0];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 1] = currentGrass.data[1];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 2] = currentGrass.data[2];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 3] = 255;
             }
             if (x > leftGrassBound-1 && x < leftCurbBound) {
-                ctx.putImageData(currentCurb, x, row);
+                buffer[(x + row*SCREEN_WIDTH)*4] = 255;
+                buffer[(x + row*SCREEN_WIDTH)*4 + 1] = currentCurb.data[1];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 2] = currentCurb.data[2];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 3] = 255;
             }
             if (x >= leftCurbBound && x <= rightCurbBound) {
-                ctx.putImageData(road, x, row);
+                buffer[(x + row*SCREEN_WIDTH)*4] = road.data[0];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 1] = road.data[1];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 2] = road.data[2];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 3] = 255;
             }
             if (x > rightCurbBound && x < rightGrassBound+1) {
-                ctx.putImageData(currentCurb, x, row);
+                buffer[(x + row*SCREEN_WIDTH)*4] = currentCurb.data[0];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 1] = currentCurb.data[1];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 2] = currentCurb.data[2];
+                buffer[(x + row*SCREEN_WIDTH)*4 + 3] = 255;
             }
             if (x > rightGrassBound && x < SCREEN_WIDTH) {
-                ctx.putImageData(currentGrass, x, row);
+                buffer[(x + row*160)*4] = currentGrass.data[0];
+                buffer[(x + row*160)*4 + 1] = currentGrass.data[1];
+                buffer[(x + row*160)*4 + 2] = currentGrass.data[2];
+                buffer[(x + row*160)*4 + 3] = 255;
             }
 
-            let carY = SCREEN_HEIGHT - carImage.height - 3;
-            let carX = SCREEN_WIDTH / 2 + (SCREEN_WIDTH * playerPosition / 2) - carImage.width/2;
 
-            ctx.drawImage(carImage, carX, carY);
 
             playerDistance += playerSpeed;
 
         }
     }
-    trackSection+=0.1;
+    ctx.putImageData(bufferData, 0, 0);
+
+    let carY = SCREEN_HEIGHT - carImage.height - 3;
+    let carX = SCREEN_WIDTH / 2 + (SCREEN_WIDTH * playerPosition / 2) - carImage.width/2;
+
+    ctx.drawImage(carImage, carX, carY);
+    trackSection+=0.01;
 }
 
 setTimeout(() => {
     setInterval(()=>{
         updateScreen();
-    }, 100);
+    }, 17);
 }, 2000);
